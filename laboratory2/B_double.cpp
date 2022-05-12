0dbdd918f93b37c0e7475700dae7822afa881e0f
Anna <91415907+dumb-anchovy@users.noreply.github.com>
2022-05-12 16:22:24 +0300

    ex B
    double doesn`t work but idk why

---
+++ laboratory2/B_double.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double close_values(double const psi[], double const pdf[], double const dv, int size){
+    int step = 1;
+    double x[size];
+    for(int i = 0; i < size; i++){
+        x[i] = psi[i] * pdf[i] * dv;
+    }
+    while(step < size){
+        for(int i = 0; i + step < size; i = i + 2 * step){
+            x[i] = x[i] +x[i + step];
+        }
+        step *= 2;
+    }
+    return x[0];
+}
+
+int main()
+{
+    double const f_pi = 3.14159265359;
+    double const f_e = 2.718281828459;
+    double T = 300;
+    unsigned int n = 10000000;
+    double left = -1000;
+    double right = 1000;
+    double v = left;
+    double dv = (right - left) / n;
+    double *psi = 0;
+    double *pdf = 0;
+    psi = new double[n];
+    pdf = new double[n];
+    for (int i = 0; i < n; i++)
+    {
+        v = v + dv;
+        psi[i] = abs(v);
+        pdf[i] = pow(f_e, (- v * v / T)) / sqrt(f_pi * T);
+    }
+    cout<< "expectation:" << sqrt(T/f_pi)<<endl;
+    cout << "result:" << close_values(psi, pdf, dv, n) << endl;
+    delete [] psi;
+    delete [] pdf;
+    return 0;
+}
---
+++ laboratory2/B_float.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float close_values(float const psi[], float const pdf[], float const dv, int size){
+    int step = 1;
+    float ans[size];
+    for(int i = 0; i < size; i++){
+        ans[i] = psi[i] * pdf[i] * dv;
+    }
+    while(step < size){
+        for(int i = 0; i + step < size; i += 2*step){
+            ans[i] = ans[i] +ans[i + step];
+        }
+        step *= 2;
+    }
+    return ans[0];
+}
+
+int main()
+{
+    float const f_pi = 3.14159265359f;
+    float const f_e = 2.718281828459f;
+    float T = 300;
+    unsigned int n = 100000;
+    float left = -100;
+    float right = 100;
+    float v = left;
+    float dv = (right - left) / n;
+    float *psi = 0;
+    float *pdf = 0;
+    psi = new float[n];
+    pdf = new float[n];
+    for (int i = 0; i < n; i++)
+    {
+        v = v + dv;
+        psi[i] = abs(v);
+        pdf[i] = pow(f_e, (- v * v / T)) / sqrt(f_pi * T);
+    }
+    cout<< "expectation:" << sqrt(T/f_pi)<<endl;
+    cout << "result:" << close_values(psi, pdf, dv, n) << endl;
+    delete [] psi;
+    delete [] pdf;
+    return 0;
+}
